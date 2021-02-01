ca15e04c5158c5b2ae2445649ae38d433f5bcbaf
supanat <supanat.ap@gmail.com>
2021-02-01 17:58:04 +0700

    edit

--- 105_lab1.5.c
+++ 105_lab1.5.c
@@ -1,4 +1,18 @@
-#include<stdio.h>
 int main()
 {
-    int a,i;
+    int a,i,n,max=0,po;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        {
+            scanf("%d",&a);
+            if(a>max)
+                {
+                    max=a;
+                    po=i;
+                }
+        }
+    printf("%d %d",po+1,max);
+
+}
