8c3a575d0d16bc1dd33e2bed9d11cab9e94ceb67
supanat <supanat.ap@gmail.com>
2021-02-01 19:11:19 +0700

    e

--- 105_lab1.6.c
+++ 105_lab1.6.c
@@ -1,39 +1,46 @@
 #include<stdio.h>
 int main()
-{   long n,m,l,i=1,foot,j;
+{
+    long n,m,l,i=1,foot,j;
     char c;
     scanf("%d %d %d",&n,&m,&l);
     fflush(stdin);
     scanf("%c",&c);
-        while(i<=l)
+    if(0<n<1000 && 0<m<1000)
+    {
+        if(0<l<100000 && m<l && n<l)
         {
+            while(i<=l)
+            {
 
-                 if(c=='L')
-                 {
-                     for(j=0;j<n;j++)
-                     {
-                         i++;
-                     }
-                     c='R';
-                 }
-                 else if(c=='R')
-                 {
-                     for(j=0;j<n;j++)
-                     {
-                         i++;
-                     }
-                     c='L';
-                 }
-                 else
+                if(c=='L')
+                {
+                    for(j=0; j<n; j++)
+                    {
+                        i++;
+                    }
+                    c='R';
+                }
+                else if(c=='R')
+                {
+                    for(j=0; j<n; j++)
+                    {
+                        i++;
+                    }
+                    c='L';
+                }
+                else
                     i++;
 
 
+            }
         }
+    }
 
-        if(c=='L')
-            printf("L");
-        else if(c=='R')
-            printf("R");
+    if(c=='L')
+        printf("L");
+    else if(c=='R')
+        printf("R");
 
     return 0;
 }
