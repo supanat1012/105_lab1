2af1796695c04d35436248795ea82df4d3ab43c3
supanat <supanat.ap@gmail.com>
2021-02-01 18:42:49 +0700

    edit

--- 105_lab1.6.c
+++ 105_lab1.6.c
@@ -1,9 +1,38 @@
 #include<stdio.h>
 int main()
-{   int n,m,l;
+{   int n,m,l,i=1,foot,j;
+    char c;
     scanf("%d %d %d",&n,&m,&l);
-    if(0<n<1000&&0<m<1000&&n+m<=l&&0<l<1000000000)
+    fflush(stdin);
+    scanf("%c",&c);
+    if(0<n<1000 && 0<m<1000 && m<l && n<l && 0<l<1000000000)
     {
-        printf("Test");
+        while(i<=l)
+        {
+
+                 if(c=='L')
+                 {
+                     for(j=0;j<n;j++)
+                     {
+                         i++;
+                     }
+                     c='R';
+                 }
+                 else if(c=='R')
+                 {
+                     for(j=0;j<n;j++)
+                     {
+                         i++;
+                     }
+                     c='L';
+                 }
+
+
+
+        }
+        if(c=='L')
+        printf("L");
+        else if(c=='R')
+        printf("R");
     }
 }
